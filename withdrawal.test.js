7d8b5a2585f0da0081a4e4fdbf10a0cd0a2819cb
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-11-15 08:05:04 +0000

    Added withdrawal class + tests for returning date and amount withdrew

---
+++ withdrawal.test.js
@@ -0,0 +1,15 @@
+const Withdrawal = require('./withdrawal')
+
+
+describe("withdrawal", () => {
+  it("returns date of withdrawal", () => {
+    const withdrawal = new Withdrawal("16/11/2022", 500)
+    expect(withdrawal.date).toEqual("16/11/2022")
+  });
+
+  it("returns amount deposited", () => {
+    const withdrawal = new Withdrawal("16/11/2022", 500)
+    expect(withdrawal.amount).toEqual(500);
+  });
+
+});
